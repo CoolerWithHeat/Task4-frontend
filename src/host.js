c1c9eb4f0dd7e03bbf67ff58ce141260cc143a5e
CoolerWithHeat <pcjoker04@gmail.com>
2023-10-12 03:02:14 +0500

    with backend integrated

--- src/host.js
+++ src/host.js
@@ -1,7 +1,8 @@
 function CurrentHost() {
-    const host = window.location.protocol + '//' + window.location.host;
+    // const host = window.location.protocol + '//' + window.location.host;
 
-    return host =='http://localhost:3000' ? 'http://127.0.0.1:8000/' : window.location.protocol + '//' + window.location.host + '/';
+    // return host =='http://localhost:3000' ? 'http://127.0.0.1:8000/' : window.location.protocol + '//' + window.location.host + '/';
+    return 'https://task4-0bdc3bba8145.herokuapp.com/';
 }
 
 export default CurrentHost
